0af6a048d07b182cc115c0bff1769bece2e672c2
Vinz2008 <vincentbidarddelanoe@gmail.com>
2023-12-30 20:38:54 +0100

    Added Readme for nvim-lua and added peek.nvim

---
+++ nvim-lua/README.md
@@ -0,0 +1,19 @@
+# Useful keybindings and commands
+
+## Keybindings
+
+### Filesystem tree (Neo-Tree)
+
+Ctrl + n : print or close folder filesystem tree
+
+## Commands
+### Markdown (peek.nvim) 
+:PeekOpen : Open a real time markdown preview
+
+### Git (vim-fugitive)
+:Git commit  
+:Git diff  
+:Git blame  
+:Git  
+
+:Gdelete : Equivalent to "git rm"
--- nvim-lua/init.lua
+++ nvim-lua/init.lua
@@ -219,6 +219,17 @@ require('lazy').setup({
         require('neo-tree').setup {}
     end,
   },
+  {
+    "toppair/peek.nvim",
+    event = { "VeryLazy" },
+    build = "deno task --quiet build:fast",
+    config = function()
+        require("peek").setup()
+        -- refer to `configuration to change defaults`
+        vim.api.nvim_create_user_command("PeekOpen", require("peek").open, {})
+        vim.api.nvim_create_user_command("PeekClose", require("peek").close, {})
+    end,
+  },
   --[[{
     "Mofiqul/dracula.nvim",
     config = function()
